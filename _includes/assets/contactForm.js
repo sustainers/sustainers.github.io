05d36f2de833130e1746dc17acb0302fb7eb3926
XhmikosR <xhmikosr@gmail.com>
2018-05-11 12:11:54 +0300

    contact form: Show a success alert on success.

--- _includes/assets/contactForm.js
+++ _includes/assets/contactForm.js
@@ -5,11 +5,14 @@
     var formEmail = document.getElementById('contactFormName');
     var formName = document.getElementById('contactFormEmail');
     var formMsg = document.getElementById('contactFormMessage');
+    var contactFormSubmit = document.getElementById('contactFormSubmit');
+    var formLog = document.getElementById('contactFormLog');
 
     contactFormSubmit.addEventListener('click', function() {
         if (formEmail.validity.valid && formName.validity.valid && formMsg.validity.valid) {
             setTimeout(function() {
-                window.location = '/';
+                form.reset();
+                formLog.style.display = 'block';
             }, 1000);
         }
     }, false);
