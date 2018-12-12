c4bc27c9f0fc6ce07717258d44f9f1d1e1539972
XhmikosR <xhmikosr@gmail.com>
2018-12-12 19:20:30 +0200

    Add 2018 files.

---
+++ summit/2018/assets/js/tickets-carousel.js
@@ -0,0 +1,67 @@
+/* eslint-env jquery */
+
+(function ($) {
+    'use strict';
+
+    var $win = $(window);
+    var $hasRun = false;
+    var $carouselItems = $('.carousel-show-three .carousel-item');
+
+    function removeCarouselItems() {
+        if ($win.width() >= 768) {
+            return;
+        }
+
+        $carouselItems.each(function (i, el) {
+            // Remove .carousel-item's first child next elements
+            $(el)
+                .children(':first-child')
+                .nextAll()
+                .remove();
+        });
+
+        $hasRun = false;
+    }
+
+    // For every slide in carousel, copy the next slide's item in the slide.
+    // Do the same for the next, next item.
+    function populateCarouselItems() {
+        if ($win.width() < 768 || $hasRun === true) {
+            return;
+        }
+
+        $carouselItems.each(function (i, el) {
+            var $next = $(el).next();
+
+            if (!$next.length) {
+                $next = $(el).siblings(':first');
+            }
+
+            $next.children(':first-child')
+                .clone()
+                .appendTo($(el));
+
+            if ($next.next().length > 0) {
+                $next.next()
+                    .children(':first-child')
+                    .clone()
+                    .appendTo($(el));
+            } else {
+                $(el)
+                    .siblings(':first')
+                    .children(':first-child')
+                    .clone()
+                    .appendTo($(el));
+            }
+        });
+
+        $hasRun = true;
+    }
+
+    populateCarouselItems();
+
+    $win.on('resize', function () {
+        removeCarouselItems();
+        populateCarouselItems();
+    });
+})(jQuery);
