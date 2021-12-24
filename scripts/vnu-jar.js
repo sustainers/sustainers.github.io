ce7afe1d71f1e60822364f755da728fbc01d906b
XhmikosR <xhmikosr@gmail.com>
2021-12-24 10:17:12 +0200

    Update scripts/vnu-jar.js from upstream

--- scripts/vnu-jar.js
+++ scripts/vnu-jar.js
@@ -2,37 +2,37 @@
 
 /*!
  * Script to run vnu-jar if Java is available.
- * Copyright 2017-2018 The Bootstrap Authors
- * Copyright 2017-2018 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * Copyright 2017-2021 The Bootstrap Authors
+ * Copyright 2017-2021 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  */
 
-'use strict';
+'use strict'
 
-var childProcess = require('child_process');
-var vnu = require('vnu-jar');
+const { execFile, spawn } = require('child_process');
+const vnu = require('vnu-jar');
 
-childProcess.exec('java -version', function(error, stdout, stderr) {
+execFile('java', ['-version'], (error, stdout, stderr) => {
     if (error) {
-        console.warn('Skipping vnu-jar test; Java is missing.');
+        console.error('Skipping vnu-jar test; Java is missing.');
         return;
     }
 
-    var is32bitJava = !stderr.match(/64-Bit/);
+    const is32bitJava = !/64-Bit/.test(stderr);
 
     // vnu-jar accepts multiple ignores joined with a `|`.
-    // Also note that the ignores are regular expressions.
-    var ignores = [
+    // Also note that the ignores are string regular expressions.
+    const ignores = [
         'This document appears to be written in.*'
     ].join('|');
 
-    var args = [
+    const args = [
         '-jar',
-        vnu,
+        `"${vnu}"`,
         '--asciiquotes',
         '--skip-non-html',
         '--Werror',
-        '--filterpattern "' + ignores + '"',
+        `--filterpattern "${ignores}"`,
         '_site/'
     ];
 
@@ -41,7 +41,7 @@ childProcess.exec('java -version', function(error, stdout, stderr) {
         args.splice(0, 0, '-Xss512k');
     }
 
-    return childProcess.spawn('java', args, {
+    return spawn('java', args, {
         shell: true,
         stdio: 'inherit'
     }).on('exit', process.exit);
